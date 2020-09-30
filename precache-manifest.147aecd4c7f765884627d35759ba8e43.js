b7ec061158f16c2630b7961c9942d4823e39e9bc
32Ivan <ivan.govic32@gmail.com>
2020-09-30 17:38:57 +0200

    Updates

---
+++ precache-manifest.147aecd4c7f765884627d35759ba8e43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0caadf3a555929607213bc1a1ded6086",
+    "url": "/face/index.html"
+  },
+  {
+    "revision": "70af2041739ff20763fa",
+    "url": "/face/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "257a0cee7ab80b788cb0",
+    "url": "/face/static/css/main.a860d20e.chunk.css"
+  },
+  {
+    "revision": "70af2041739ff20763fa",
+    "url": "/face/static/js/2.1240a284.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face/static/js/2.1240a284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257a0cee7ab80b788cb0",
+    "url": "/face/static/js/main.81ebabc8.chunk.js"
+  },
+  {
+    "revision": "2166077e79018d9ea408",
+    "url": "/face/static/js/runtime-main.37345edd.js"
+  },
+  {
+    "revision": "6addb86f038ba4db2af1c96324947a85",
+    "url": "/face/static/media/face.6addb86f.png"
+  }
+]);
